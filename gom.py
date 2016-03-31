a6cb8e49cece2749f797fe108445038b4e097399
Chris Oaks <chrisoaks@gmail.com>
2016-03-30 20:27:26 -0400

    updated to pull games

--- gom.py
+++ gom.py
@@ -1,8 +1,20 @@
+# Imports from requests, pprint, and BeautifulSoup
+
 import requests
 import pprint
 from bs4 import BeautifulSoup
-pd = {} #player dictionary
+import ast
+
+# pd stands for player dictioray, a hashtable of players as in:
+# {Rank: 'Player', ...}
+pd = {}
+
+# A global variable needed to count each name only once.
 searchednames = []
+
+# List of turns
+turnlist = []
+
 def getfrom(url,n):
     
     r = requests.get(url)
@@ -22,7 +34,7 @@ def getfrom(url,n):
         pd[rank] = name
         pp = pprint.PrettyPrinter(indent=4)
         pp.pprint(pd)
-    
+
     except AttributeError:
         print 'ae'
     topname = pd[sorted(pd.keys())[-1]]
@@ -33,13 +45,66 @@ def getfrom(url,n):
         if topname not in searchednames:
             searchednames.append(topname)
             break
-    
     print topname
     newurl = 'http://www.playok.com/en/stat.phtml?gid=gm&uid=' + topname + '&sk=3&so=2'
     if n - 1 !=0:
         getfrom(newurl, n-1)
+def getGameLength(S):
+    dotfound = False
+    
+    for x in S[::-1]:
+        if x == '.':
+            dotfound = True
+        if x == ' ' and dotfound == True:
+            return 0 # fix later
 
-getfrom('http://www.playok.com/en/stat.phtml?gid=gm&uid=gregi73&sk=3&so=2',100)
+def getTupleFrom(S):
+    return (S[0],S[1])
 
+def getFromTxt(url):
+    r = requests.get(url)
+    SplitList = r.content.split('1. ',1)
+    S = SplitList[1]
+    
+    print S.split()
+    S = S.translate(None, '.')
+    print S.split()
+    print [int(s) for s in S.split() if s.isdigit()]
+    #tuple1 = getTupleFrom(S.split()[0])
+    #tuple2 = getTupleFrom(S.split()[1])
+    #print 'tuple1 =' + str(tuple1)
+    #print 'tuple2 =' + str(tuple2)
+    #turnlist = [(tuple1),(tuple2)]
+    print 'turnlist =' + str(turnlist)
+    bfirstplayersturn = True
+    firsttuple = ()
+    secondtuple = ()
+    for s in S.split():
+        if s.isdigit():
+            print int(s)
+            continue
+        if bfirstplayersturn == True:
+            bfirstplayersturn = False
+            firsttuple = getTupleFrom(s)
+        else:
+            bfirstplayersturn = True
+            secondtuple = getTupleFrom(s)
+            turnlist.append(((firsttuple),(secondtuple)))
+    print turnlist
+            
+            
+f = open('workfile', 'r')
+pd = ast.literal_eval(f.read()) # This is temporary, should be using pickle
+print pd[2041]
+getFromTxt('http://www.playok.com/en/game.phtml/117844329.txt?gm')
 
+#getfrom('http://www.playok.com/en/stat.phtml?gid=gm&uid=gregi73&sk=3&so=2',100)
+
+                       
+#game structure
+#list of turns
+#    tuple
+#        tuple (b, 2)
+#        tuple (d, 2)
+#[((b,2),(d,2)),((f,3),(h,1))]
 
